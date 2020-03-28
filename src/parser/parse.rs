8fee16b9f62230b947cb3f98782afff5deea825c
René Rössler <rene@freshx.de>
2020-03-28 02:03:54 +0100

    code review

--- src/parser/parse.rs
+++ src/parser/parse.rs
@@ -1,6 +1,6 @@
-use super::types::{Position, Range, ParseError, ParseResult};
 use super::tokenize::TokenType;
 use super::tree::{Entry, EntryType, Key, Tree};
+use super::types::{ParseError, ParseResult, Position, Range};
 use std::collections::HashMap;
 
 fn pre_calculate_positions(tokens: &[TokenType]) -> HashMap<usize, usize> {
@@ -132,7 +132,7 @@ fn handle_array(
     let (mut json_array, range) = create_new_array(&tokens);
     let mut skip = 0;
 
-    let result: Result<Vec<()>, ParseError> = tokens[1..(tokens.len() - 1)]
+    tokens[1..(tokens.len() - 1)]
         .iter()
         .enumerate()
         .map(|(idx, token)| {
@@ -143,45 +143,51 @@ fn handle_array(
             match token {
                 TokenType::ObjectOpen(_) => {
                     let next_pos = idx + pre_pos + 1;
-                    if let Some(close_idx) = pre_calc.get(&next_pos) {
-                        if let Ok((hash, range)) = handle_object(
-                            tree,
-                            &tokens[idx + 1..=*close_idx - pre_pos],
-                            pre_calc,
-                            next_pos,
-                        ) {
-                            skip = *close_idx - next_pos;
-                            tree.entries.push(Entry {
-                                key: None,
-                                range,
-                                entry_type: EntryType::JSONObject(hash),
-                            });
-                            json_array.push(tree.entries.len() - 1);
-                            return Ok(());
-                        }
-                    }
-                    Err(ParseError::MissingObjectBrace)
+                    pre_calc
+                        .get(&next_pos)
+                        .and_then(|close_idx| {
+                            handle_object(
+                                tree,
+                                &tokens[idx + 1..=*close_idx - pre_pos],
+                                pre_calc,
+                                next_pos,
+                            )
+                            .ok()
+                            .map(|(hash, range)| {
+                                skip = *close_idx - next_pos;
+                                tree.entries.push(Entry {
+                                    key: None,
+                                    range,
+                                    entry_type: EntryType::JSONObject(hash),
+                                });
+                                json_array.push(tree.entries.len() - 1);
+                            })
+                        })
+                        .ok_or(ParseError::MissingObjectBrace)
                 }
                 TokenType::ArrayOpen(_) => {
                     let next_pos = idx + pre_pos + 1;
-                    if let Some(close_idx) = pre_calc.get(&next_pos) {
-                        if let Ok((array_vec, range)) = handle_array(
-                            tree,
-                            &tokens[idx + 1..=*close_idx - pre_pos],
-                            pre_calc,
-                            next_pos,
-                        ) {
-                            skip = *close_idx - next_pos;
-                            tree.entries.push(Entry {
-                                key: None,
-                                range,
-                                entry_type: EntryType::JSONArray(array_vec),
-                            });
-                            json_array.push(tree.entries.len() - 1);
-                            return Ok(());
-                        }
-                    }
-                    Err(ParseError::MissingArrayBrace)
+                    pre_calc
+                        .get(&next_pos)
+                        .and_then(|close_idx| {
+                            handle_array(
+                                tree,
+                                &tokens[idx + 1..=*close_idx - pre_pos],
+                                pre_calc,
+                                next_pos,
+                            )
+                            .ok()
+                            .map(|(array_vec, range)| {
+                                skip = *close_idx - next_pos;
+                                tree.entries.push(Entry {
+                                    key: None,
+                                    range,
+                                    entry_type: EntryType::JSONArray(array_vec),
+                                });
+                                json_array.push(tree.entries.len() - 1);
+                            })
+                        })
+                        .ok_or(ParseError::MissingArrayBrace)
                 }
                 _ => {
                     if let Some(primitive) = handle_primitives(&token, None) {
@@ -193,12 +199,8 @@ fn handle_array(
                 }
             }
         })
-        .collect();
-
-    match result {
-        Ok(_) => Ok((json_array, range)),
-        Err(e) => Err(e),
-    }
+        .collect::<Result<Vec<()>, ParseError>>()
+        .map(|_| (json_array, range))
 }
 
 type ObjectParseResult = ParseResult<(HashMap<String, (usize, usize)>, Range)>;
@@ -214,7 +216,7 @@ fn handle_object(
     let (mut json_object, range) = create_new_object(&tokens);
     let mut skip = 0;
 
-    let result: Result<Vec<()>, ParseError> = tokens[1..(tokens.len() - 1)]
+    tokens[1..(tokens.len() - 1)]
         .iter()
         .enumerate()
         .map(|(idx, token)| {
@@ -240,85 +242,95 @@ fn handle_object(
                         key_pos = tree.keys.len() - 1;
                         return Ok(());
                     }
-                    if let Some(primitive) = handle_primitives(&token, Some(key_pos)) {
-                        if let Some(key) = tree.keys.get(key_pos) {
-                            tree.entries.push(primitive);
-                            json_object.insert(key.name.clone(), (key_pos, tree.entries.len() - 1));
-                            return Ok(());
-                        }
-                    }
-                    Err(ParseError::Error)
+                    handle_primitives(&token, Some(key_pos))
+                        .and_then(|primitive| {
+                            tree.keys
+                                .get(key_pos)
+                                .map(|key| key.name.clone())
+                                .map(|name| {
+                                    tree.entries.push(primitive);
+                                    json_object.insert(name, (key_pos, tree.entries.len() - 1));
+                                })
+                        })
+                        .ok_or(ParseError::Error)
                 }
                 TokenType::ObjectOpen(_) => {
                     let next_pos = idx + pre_pos + 1;
-                    if let Some(close_idx) = pre_calc.get(&next_pos) {
-                        if let Ok((hash, range)) = handle_object(
-                            tree,
-                            &tokens[idx + 1..=*close_idx - pre_pos],
-                            pre_calc,
-                            next_pos,
-                        ) {
-                            skip = *close_idx - next_pos;
-                            if let Some(key) = tree.keys.get(key_pos) {
-                                tree.entries.push(Entry {
-                                    key: Some(key_pos),
-                                    range,
-                                    entry_type: EntryType::JSONObject(hash),
-                                });
-                                json_object
-                                    .insert(key.name.clone(), (key_pos, tree.entries.len() - 1));
+                    pre_calc
+                        .get(&next_pos)
+                        .and_then(|close_idx| {
+                            handle_object(
+                                tree,
+                                &tokens[idx + 1..=*close_idx - pre_pos],
+                                pre_calc,
+                                next_pos,
+                            )
+                            .ok()
+                            .and_then(|(hash, range)| {
+                                skip = *close_idx - next_pos;
+                                tree.keys
+                                    .get(key_pos)
+                                    .map(|key| key.name.clone())
+                                    .map(|name| {
+                                        tree.entries.push(Entry {
+                                            key: Some(key_pos),
+                                            range,
+                                            entry_type: EntryType::JSONObject(hash),
+                                        });
+                                        json_object.insert(name, (key_pos, tree.entries.len() - 1));
 
-                                is_key = true;
-                                return Ok(());
-                            }
-                        }
-                    }
-
-                    Err(ParseError::MissingObjectBrace)
+                                        is_key = true;
+                                    })
+                            })
+                        })
+                        .ok_or(ParseError::MissingObjectBrace)
                 }
                 TokenType::ArrayOpen(_) => {
                     let next_pos = idx + pre_pos + 1;
-                    if let Some(close_idx) = pre_calc.get(&next_pos) {
-                        if let Ok((array_vec, range)) = handle_array(
-                            tree,
-                            &tokens[idx + 1..=*close_idx - pre_pos],
-                            pre_calc,
-                            next_pos,
-                        ) {
-                            skip = *close_idx - next_pos;
-                            if let Some(key) = tree.keys.get(key_pos) {
-                                tree.entries.push(Entry {
-                                    key: Some(key_pos),
-                                    range,
-                                    entry_type: EntryType::JSONArray(array_vec),
-                                });
-                                json_object
-                                    .insert(key.name.clone(), (key_pos, tree.entries.len() - 1));
-                                return Ok(());
-                            }
-                        }
-                    }
-
-                    Err(ParseError::MissingArrayBrace)
+                    pre_calc
+                        .get(&next_pos)
+                        .and_then(|close_idx| {
+                            handle_array(
+                                tree,
+                                &tokens[idx + 1..=*close_idx - pre_pos],
+                                pre_calc,
+                                next_pos,
+                            )
+                            .ok()
+                            .and_then(|(array_vec, range)| {
+                                skip = *close_idx - next_pos;
+                                tree.keys
+                                    .get(key_pos)
+                                    .map(|key| key.name.clone())
+                                    .map(|name| {
+                                        tree.entries.push(Entry {
+                                            key: Some(key_pos),
+                                            range,
+                                            entry_type: EntryType::JSONArray(array_vec),
+                                        });
+                                        json_object.insert(name, (key_pos, tree.entries.len() - 1));
+                                    })
+                            })
+                        })
+                        .ok_or(ParseError::MissingArrayBrace)
                 }
                 _ => {
-                    if let Some(primitive) = handle_primitives(&token, Some(key_pos)) {
-                        if let Some(key) = tree.keys.get(key_pos) {
-                            tree.entries.push(primitive);
-                            json_object.insert(key.name.clone(), (key_pos, tree.entries.len() - 1));
-                        }
-                    }
+                    handle_primitives(&token, Some(key_pos)).map(|primitive| {
+                        tree.keys
+                            .get(key_pos)
+                            .map(|key| key.name.clone())
+                            .map(|name| {
+                                tree.entries.push(primitive);
+                                json_object.insert(name, (key_pos, tree.entries.len() - 1));
+                            })
+                    });
 
                     Ok(())
                 }
             }
         })
-        .collect();
-
-    match result {
-        Ok(_) => Ok((json_object, range)),
-        Err(e) => Err(e),
-    }
+        .collect::<Result<Vec<()>, ParseError>>()
+        .map(|_| (json_object, range))
 }
 
 pub fn parse_json(tokens: &[TokenType]) -> ParseResult<Tree> {
@@ -328,18 +340,13 @@ pub fn parse_json(tokens: &[TokenType]) -> ParseResult<Tree> {
     };
 
     let pre_calc = pre_calculate_positions(tokens);
-    if pre_calc.get(&0).is_none() {
-        return Err(ParseError::MissingObjectBrace);
-    }
-    match handle_object(&mut tree, tokens, &pre_calc, 0) {
-        Ok((hash, range)) => {
-            tree.entries.push(Entry {
-                key: None,
-                range,
-                entry_type: EntryType::JSONObject(hash),
-            });
-            Ok(tree)
-        }
-        Err(e) => Err(e),
-    }
+    pre_calc.get(&0).ok_or(ParseError::MissingObjectBrace)?;
+    handle_object(&mut tree, tokens, &pre_calc, 0).map(|(hash, range)| {
+        tree.entries.push(Entry {
+            key: None,
+            range,
+            entry_type: EntryType::JSONObject(hash),
+        });
+        tree
+    })
 }
--- src/parser/tokenize.rs
+++ src/parser/tokenize.rs
@@ -27,25 +27,25 @@ fn handle_defaults(c: char, tokens: &mut Vec<TokenType>, pos: Position) {
         '{' => {
             tokens.push(TokenType::ObjectOpen(Range {
                 start: pos,
-                end: pos.add(0, 1),
+                end: pos + Position::new(0, 1, 1),
             }));
         }
         '}' => {
             tokens.push(TokenType::ObjectClose(Range {
                 start: pos,
-                end: pos.add(0, 1),
+                end: pos + Position::new(0, 1, 1),
             }));
         }
         '[' => {
             tokens.push(TokenType::ArrayOpen(Range {
                 start: pos,
-                end: pos.add(0, 1),
+                end: pos + Position::new(0, 1, 1),
             }));
         }
         ']' => {
             tokens.push(TokenType::ArrayClose(Range {
                 start: pos,
-                end: pos.add(0, 1),
+                end: pos + Position::new(0, 1, 1),
             }));
         }
         ',' => {
@@ -70,122 +70,122 @@ pub fn tokenize(string: &str, tokens: &mut Vec<TokenType>) {
         if let '\n' = c {
             current_line += 1;
             current_char = 0;
-        } else {
-            match &current_type {
-                Some(t) => {
-                    match t {
-                        CurrentTokenType::String => {
-                            if !escaped && c == '\\' {
-                                escaped = true;
-                                concat_string.push(c);
-                            } else if !escaped && c == '"' {
-                                tokens.push(TokenType::String(
-                                    Range {
-                                        start: current_type_start.add(0, 1),
-                                        end: Position::new(current_line, current_char, pos),
-                                    },
-                                    concat_string.to_owned(),
-                                ));
-                                current_type = None;
-                                concat_string = String::new();
-                            } else {
-                                escaped = false;
-                                concat_string.push(c);
-                            }
+            return;
+        }
+        match &current_type {
+            Some(t) => {
+                match t {
+                    CurrentTokenType::String => {
+                        if !escaped && c == '\\' {
+                            escaped = true;
+                            concat_string.push(c);
+                        } else if !escaped && c == '"' {
+                            tokens.push(TokenType::String(
+                                Range {
+                                    start: current_type_start + Position::new(0, 1, 1),
+                                    end: Position::new(current_line, current_char, pos),
+                                },
+                                concat_string.to_owned(),
+                            ));
+                            current_type = None;
+                            concat_string = String::new();
+                        } else {
+                            escaped = false;
+                            concat_string.push(c);
                         }
-                        CurrentTokenType::Number => {
-                            if !c.is_digit(10) && c != '.' {
-                                if let Ok(int) = concat_string.parse::<i64>() {
-                                    tokens.push(TokenType::Int(
-                                        Range {
-                                            start: current_type_start,
-                                            end: Position::new(current_line, current_char, pos),
-                                        },
-                                        int,
-                                    ));
-                                } else if let Ok(float) = concat_string.parse::<f64>() {
-                                    tokens.push(TokenType::Float(
-                                        Range {
-                                            start: current_type_start,
-                                            end: Position::new(current_line, current_char, pos),
-                                        },
-                                        float,
-                                    ));
-                                }
-                                current_type = None;
-                                concat_string = String::new();
-                                handle_defaults(
-                                    c,
-                                    tokens,
-                                    Position::new(current_line, current_char, pos),
-                                );
-                            } else {
-                                concat_string.push(c);
-                            }
-                        }
-                        CurrentTokenType::Bool => {
-                            if concat_string == "true" || concat_string == "false" {
-                                tokens.push(TokenType::Bool(
+                    }
+                    CurrentTokenType::Number => {
+                        if !c.is_digit(10) && c != '.' {
+                            if let Ok(int) = concat_string.parse::<i64>() {
+                                tokens.push(TokenType::Int(
                                     Range {
                                         start: current_type_start,
                                         end: Position::new(current_line, current_char, pos),
                                     },
-                                    concat_string.parse::<bool>().unwrap(),
+                                    int,
                                 ));
-                                current_type = None;
-                                concat_string = String::new();
-                                handle_defaults(
-                                    c,
-                                    tokens,
-                                    Position::new(current_line, current_char, pos),
-                                );
-                            } else {
-                                concat_string.push(c);
+                            } else if let Ok(float) = concat_string.parse::<f64>() {
+                                tokens.push(TokenType::Float(
+                                    Range {
+                                        start: current_type_start,
+                                        end: Position::new(current_line, current_char, pos),
+                                    },
+                                    float,
+                                ));
                             }
+                            current_type = None;
+                            concat_string = String::new();
+                            handle_defaults(
+                                c,
+                                tokens,
+                                Position::new(current_line, current_char, pos),
+                            );
+                        } else {
+                            concat_string.push(c);
                         }
-                        CurrentTokenType::Null => {
-                            if concat_string == "null" {
-                                tokens.push(TokenType::Null(Range {
+                    }
+                    CurrentTokenType::Bool => {
+                        if concat_string == "true" || concat_string == "false" {
+                            tokens.push(TokenType::Bool(
+                                Range {
                                     start: current_type_start,
                                     end: Position::new(current_line, current_char, pos),
-                                }));
-                                current_type = None;
-                                concat_string = String::new();
-                                handle_defaults(
-                                    c,
-                                    tokens,
-                                    Position::new(current_line, current_char, pos),
-                                );
-                            } else {
-                                concat_string.push(c);
-                            }
+                                },
+                                concat_string.parse::<bool>().unwrap(),
+                            ));
+                            current_type = None;
+                            concat_string = String::new();
+                            handle_defaults(
+                                c,
+                                tokens,
+                                Position::new(current_line, current_char, pos),
+                            );
+                        } else {
+                            concat_string.push(c);
                         }
-                    };
+                    }
+                    CurrentTokenType::Null => {
+                        if concat_string == "null" {
+                            tokens.push(TokenType::Null(Range {
+                                start: current_type_start,
+                                end: Position::new(current_line, current_char, pos),
+                            }));
+                            current_type = None;
+                            concat_string = String::new();
+                            handle_defaults(
+                                c,
+                                tokens,
+                                Position::new(current_line, current_char, pos),
+                            );
+                        } else {
+                            concat_string.push(c);
+                        }
+                    }
+                };
+            }
+            None => match c {
+                '"' => {
+                    current_type = Some(CurrentTokenType::String);
+                    current_type_start = Position::new(current_line, current_char, pos);
                 }
-                None => match c {
-                    '"' => {
-                        current_type = Some(CurrentTokenType::String);
-                        current_type_start = Position::new(current_line, current_char, pos);
-                    }
-                    'n' => {
-                        current_type = Some(CurrentTokenType::Null);
-                        current_type_start = Position::new(current_line, current_char, pos);
-                        concat_string.push(c);
-                    }
-                    't' | 'f' => {
-                        current_type = Some(CurrentTokenType::Bool);
-                        current_type_start = Position::new(current_line, current_char, pos);
-                        concat_string.push(c);
-                    }
-                    '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
-                        current_type = Some(CurrentTokenType::Number);
-                        current_type_start = Position::new(current_line, current_char, pos);
-                        concat_string.push(c);
-                    }
-                    _ => handle_defaults(c, tokens, Position::new(current_line, current_char, pos)),
-                },
-            };
-            current_char += 1;
-        }
+                'n' => {
+                    current_type = Some(CurrentTokenType::Null);
+                    current_type_start = Position::new(current_line, current_char, pos);
+                    concat_string.push(c);
+                }
+                't' | 'f' => {
+                    current_type = Some(CurrentTokenType::Bool);
+                    current_type_start = Position::new(current_line, current_char, pos);
+                    concat_string.push(c);
+                }
+                '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
+                    current_type = Some(CurrentTokenType::Number);
+                    current_type_start = Position::new(current_line, current_char, pos);
+                    concat_string.push(c);
+                }
+                _ => handle_defaults(c, tokens, Position::new(current_line, current_char, pos)),
+            },
+        };
+        current_char += 1;
     });
 }
